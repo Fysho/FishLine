c03576e40cb62ac16f9b78519806388401713ba1
A-G-K <agk8097@gmail.com>
2020-03-29 03:05:56 +1100

    Add template for generic IBodyController and MotionBody component

---
+++ Assets/IBodyController.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public interface IBodyController
+{
+    Vector2 ExternalVelocity
+    {
+        get;
+        set;
+    }
+}
---
+++ Assets/MotionBody.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+
+public class MotionBody : MonoBehaviour
+{
+    private Vector2 velocity;
+    
+    private void FixedUpdate()
+    {
+        
+    }
+}
